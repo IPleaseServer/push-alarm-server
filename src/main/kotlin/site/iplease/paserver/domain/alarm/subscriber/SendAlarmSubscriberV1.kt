eb5d917e60598d9d6723da25f30e187589f36dbc
raul <develop.raul@gmail.com>
2022-06-13 14:41:52 +0900

    [ADD] RabbitMqListener작성 및 이에 필요한 객체 작성
    - MessageType 작성
    - SendAlarmSubscriberV1에 Service annotation 추가

--- src/main/kotlin/site/iplease/paserver/domain/alarm/subscriber/SendAlarmSubscriberV1.kt
+++ src/main/kotlin/site/iplease/paserver/domain/alarm/subscriber/SendAlarmSubscriberV1.kt
@@ -1,11 +1,13 @@
 package site.iplease.paserver.domain.alarm.subscriber
 
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import site.iplease.paserver.domain.alarm.service.PushAlarmService
 import site.iplease.paserver.domain.alarm.util.SendAlarmConverter
 import site.iplease.paserver.global.alarm.data.message.SendAlarmMessage
 import site.iplease.paserver.global.alarm.subscriber.SendAlarmSubscriber
 
+@Service
 class SendAlarmSubscriberV1(
     private val sendAlarmConverter: SendAlarmConverter,
     private val pushAlarmService: PushAlarmService
