ed721e927bc4a1d41d360b4f91eadb402071df73
raul <develop.raul@gmail.com>
2022-08-11 17:29:22 +0900

    [ADD] FcmServiceImpl에 추가된 스팩 구현 및 이에 필요한 스팩 추가
    - FcmTokenRepository 스팩 추가

--- src/main/kotlin/site/iplease/paserver/domain/fcm/repository/FcmTokenRepository.kt
+++ src/main/kotlin/site/iplease/paserver/domain/fcm/repository/FcmTokenRepository.kt
@@ -5,5 +5,6 @@ import site.iplease.paserver.domain.fcm.data.entity.FcmToken
 
 interface FcmTokenRepository {
     fun findByAccountId(receiverId: Long): Mono<FcmToken>
-
+    fun save(fcmToken: FcmToken): Mono<FcmToken>
+    fun deleteById(issuerId: Long): Mono<Unit>
 }
--- src/main/kotlin/site/iplease/paserver/domain/fcm/service/FcmServiceImpl.kt
+++ src/main/kotlin/site/iplease/paserver/domain/fcm/service/FcmServiceImpl.kt
@@ -3,6 +3,9 @@ package site.iplease.paserver.domain.fcm.service
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import site.iplease.paserver.domain.fcm.data.entity.FcmToken
+import site.iplease.paserver.domain.fcm.repository.FcmTokenRepository
 import site.iplease.paserver.domain.fcm.util.FcmAccessTokenUtil
 import site.iplease.paserver.domain.fcm.util.FcmMessageConverter
 import site.iplease.paserver.global.fcm.config.FcmProperties
@@ -12,6 +15,7 @@ class FcmServiceImpl(
     private val fcmProperties: FcmProperties,
     private val fcmMessageConverter: FcmMessageConverter,
     private val fcmAccessTokenUtil: FcmAccessTokenUtil,
+    private val fcmTokenRepository: FcmTokenRepository,
     private val webClientBuilder: WebClient.Builder
 ): FcmService {
     override fun sendFcm(token: String, title: String, content: String) {
@@ -28,4 +32,8 @@ class FcmServiceImpl(
                     .retrieve().bodyToMono(String::class.java)
             }.block()!!
     }
+
+    override fun addToken(issuerId: Long, token: String): Mono<Unit> = fcmTokenRepository.save(FcmToken(issuerId, token)).map{ }
+
+    override fun removeToken(issuerId: Long): Mono<Unit> = fcmTokenRepository.deleteById(issuerId)
 }
