dfa69e2f8370b64d5578157aa6bc0ba417738db3
raul <develop.raul@gmail.com>
2022-08-11 17:30:56 +0900

    [ADD] RedisFcmTokenRepository 추가된 스팩 구현

--- src/main/kotlin/site/iplease/paserver/domain/fcm/repository/RedisFcmTokenRepository.kt
+++ src/main/kotlin/site/iplease/paserver/domain/fcm/repository/RedisFcmTokenRepository.kt
@@ -14,5 +14,13 @@ class RedisFcmTokenRepository(
     override fun findByAccountId(receiverId: Long): Mono<FcmToken> =
         redisTemplate.opsForValue().get(formatKey(receiverId))
 
+    override fun save(fcmToken: FcmToken): Mono<FcmToken> =
+        redisTemplate.opsForValue()
+            .set(formatKey(fcmToken.accountId), fcmToken)
+            .map { fcmToken }
+
+    override fun deleteById(issuerId: Long): Mono<Unit> =
+        redisTemplate.delete(formatKey(issuerId)).map{ }
+
     private fun formatKey(receiverId: Long) = "${fcmProperties.redisKeyPrefix}_$receiverId"
 }
