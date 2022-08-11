b0496415bc6fcc20b8b8012e87687efdfd51a35e
raul <develop.raul@gmail.com>
2022-08-11 16:01:11 +0900

    [ADD] EventDrivenFcmPushAlarmStrategy 구현 및 이에 필요한 수정사항 적용
    - SendFcmRequestedEvent 작성
    - DummyFcmPushAlarmStrategy 비활성화 (Deprecating)

--- src/main/kotlin/site/iplease/paserver/global/fcm/strategy/DummyFcmPushAlarmStrategy.kt
+++ src/main/kotlin/site/iplease/paserver/global/fcm/strategy/DummyFcmPushAlarmStrategy.kt
@@ -1,16 +1,16 @@
 package site.iplease.paserver.global.fcm.strategy
 
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
-@Component
+//@Component
+@Deprecated("use event driven or another implementation")
 class DummyFcmPushAlarmStrategy: FcmPushAlarmStrategy {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    override fun sendAlarm(title: String, description: String): Mono<Unit> {
-        logger.info("sendFCMAlarm: title=$title, description=$description")
+    override fun sendAlarm(receiverId: Long, title: String, description: String): Mono<Unit> {
+        logger.info("sendFCMAlarm: receiverId=$receiverId, title=$title, description=$description")
         return Unit.toMono()
     }
 }
