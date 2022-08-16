e6f869f2a53309db50762199da39cf32812cda0e
raul <develop.raul@gmail.com>
2022-08-16 19:55:25 +0900

    [ADD 오류 로깅로직 추가

--- src/main/kotlin/site/iplease/paserver/domain/alarm/subscriber/SendAlarmSubscriberV1.kt
+++ src/main/kotlin/site/iplease/paserver/domain/alarm/subscriber/SendAlarmSubscriberV1.kt
@@ -1,5 +1,6 @@
 package site.iplease.paserver.domain.alarm.subscriber
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import site.iplease.paserver.domain.alarm.service.PushAlarmService
@@ -12,10 +13,15 @@ class SendAlarmSubscriberV1(
     private val sendAlarmConverter: SendAlarmConverter,
     private val pushAlarmService: PushAlarmService
 ): SendAlarmSubscriber {
+    private val logger = LoggerFactory.getLogger(this::class.java)
     override fun subscribe(message: SendAlarmMessage) {
         sendAlarmConverter.convert(message)
             .flatMap { data -> pushAlarmService.sendAlarm(data) }
-            .onErrorResume { Mono.empty() }
+            .doOnError { throwable ->
+                logger.error("알람송신로직 수행중 오류가 발생하였습니다!")
+                logger.error("type: ${throwable::class.simpleName}")
+                logger.error("exception: ${throwable.localizedMessage}")
+            }.onErrorResume { Mono.empty() }
             .block()
     }
 }
