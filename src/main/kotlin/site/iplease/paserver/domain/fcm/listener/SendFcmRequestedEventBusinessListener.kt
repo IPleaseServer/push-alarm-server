e38174534babd710728e902d60c1d732c462e415
raul <develop.raul@gmail.com>
2022-08-16 19:55:10 +0900

    [FIX] 잘못된 Event Listening로직 수정

--- src/main/kotlin/site/iplease/paserver/domain/fcm/listener/SendFcmRequestedEventBusinessListener.kt
+++ src/main/kotlin/site/iplease/paserver/domain/fcm/listener/SendFcmRequestedEventBusinessListener.kt
@@ -3,6 +3,7 @@ package site.iplease.paserver.domain.fcm.listener
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import site.iplease.paserver.domain.email.data.event.SendEmailRequestedEvent
+import site.iplease.paserver.domain.fcm.data.event.SendFcmRequestedEvent
 import site.iplease.paserver.domain.fcm.service.FcmService
 import site.iplease.paserver.domain.fcm.util.FcmTokenResolver
 
@@ -12,7 +13,7 @@ class SendFcmRequestedEventBusinessListener(
     private val fcmService: FcmService
 ) {
     @EventListener
-    fun handle(event: SendEmailRequestedEvent) {
+    fun handle(event: SendFcmRequestedEvent) {
         fcmTokenResolver.resolve(event.receiverId)
             .map { token -> fcmService.sendFcm(token = token, title = event.title, content = event.content) }
             .block()!!
